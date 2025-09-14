c7dfe0fcbbab533ba8f7ff54ceb67df32452c86d
KriStiA <Valve>
2025-09-14 16:04:54 +0300

    Реализовать соединение с Кафкой

--- telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
@@ -6,10 +6,12 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 
 import java.util.Properties;
 
+@Component
 public class KafkaEventProducer<T extends SpecificRecordBase> {
     private final Producer<String, T> producer;
 
