c1cd6d79cc8dc791d3a56d9ddeccd4a294763f15
KriStiA <Valve>
2025-09-15 02:50:54 +0300

    Реализовать соединение с Кафкой

--- telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
@@ -7,14 +7,18 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 
+import java.time.Duration;
 import java.util.Properties;
+import org.slf4j.Logger;
 
 @Component
 public class KafkaEventProducer<T extends SpecificRecordBase> {
     private final Producer<String, T> producer;
+    private static final Logger logger = LoggerFactory.getLogger(KafkaEventProducer.class);
 
     public KafkaEventProducer() {
         Properties config = new Properties();
@@ -27,6 +31,7 @@ public class KafkaEventProducer<T extends SpecificRecordBase> {
     }
 
     public void send(String topic, T event) {
+        logger.info("Отправляем в Kafka событие: {}", event);
         ProducerRecord<String, T> record = new ProducerRecord<>(topic, null, event);
         producer.send(record);
     }
@@ -34,7 +39,7 @@ public class KafkaEventProducer<T extends SpecificRecordBase> {
     @PreDestroy
     public void close() {
         producer.flush();
-        producer.close();
+        producer.close(Duration.ofSeconds(10));
     }
 
 }
