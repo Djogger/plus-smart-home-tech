75dac0489e12e9f5c0a362e8ec8726fecfc12345
KriStiA <Valve>
2025-09-14 16:30:17 +0300

    Реализовать соединение с Кафкой

--- telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/configuration/KafkaEventProducer.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.configuration;
 
+import jakarta.annotation.PreDestroy;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -12,22 +13,28 @@ import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 import java.util.Properties;
 
 @Component
-public class KafkaEventProducer<T extends SpecificRecordBase> {
-    private final Producer<String, T> producer;
+public class KafkaEventProducer {
+    private final Producer<String, SpecificRecordBase> producer;
 
     public KafkaEventProducer() {
         Properties config = new Properties();
 
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
 
         producer = new KafkaProducer<>(config);
     }
 
-    public void send(String topic, T event) {
-        ProducerRecord<String, T> record = new ProducerRecord<>(topic, null, event);
+    public void send(String topic, SpecificRecordBase event) {
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(topic, null, event);
         producer.send(record);
+    }
+
+    @PreDestroy
+    public void close() {
+        producer.flush();
+        producer.close();
     }
 
 }
