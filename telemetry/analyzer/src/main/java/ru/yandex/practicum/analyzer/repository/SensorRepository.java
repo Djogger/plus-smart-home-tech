ccaa5f48188e367157d98828c5a7c9e60025c5ab
KriStiA <Valve>
2025-09-24 17:02:26 +0300

    Реализовать модуль анализатора

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/repository/SensorRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/repository/SensorRepository.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.analyzer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.analyzer.model.Sensor;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SensorRepository extends JpaRepository<Sensor, String> {
     Optional<Sensor> findByIdAndHubId(String id, String hubId);
     void deleteByIdAndHubId(String id, String hubId);
